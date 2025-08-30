31a31daf8fea3a656a2665ce800088809975baa9
alialiearan-cell <ali.aliearan@gmail.com>
2025-08-30 22:15:47 +0330

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+from scapy.all import IP, TCP, send  
+ip = IP(dst="109.176.239.69")
+tcp = TCP(sport=randint(2000,60000), dport=443, flags="S") 
+
+for i in range(10):
+  send(ip/tcp)
+print("finish")
